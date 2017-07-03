d4cf264fc6b19062bce3db8315bce17f871a3ae0
Dalton Spillman <dalton_spillman@yahoo.com>
2017-07-02 22:02:38 -0500

    Added FastBitmap Got closer to determining objects

---
+++ Part 2/ObjectTracker/FastBitmap.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ObjectTracker
+{
+    public unsafe class FastBitmap
+    {
+        Bitmap bitmap;
+
+        // three elements used for MakeGreyUnsafe
+        int dataWidth;
+        BitmapData bitmapData = null;
+        Byte* pBase = null;
+        PixelData[,] pixels;
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public FastBitmap(Bitmap bitmap)
+        {
+            if (bitmap != null)
+            {
+                this.bitmap = bitmap.Clone() as Bitmap;
+            }
+        }
+
+        public void Dispose()
+        {
+            bitmap.Dispose();
+        }
+
+        public Bitmap Bitmap
+        {
+            get
+            {
+                return (bitmap);
+            }
+        }
+
+        private Point PixelSize
+        {
+            get
+            {
+                GraphicsUnit unit = GraphicsUnit.Pixel;
+                RectangleF bounds = bitmap.GetBounds(ref unit);
+
+                return new Point((int)bounds.Width, (int)bounds.Height);
+            }
+        }
+
+        public void LockBitmap()
+        {
+            GraphicsUnit unit = GraphicsUnit.Pixel;
+            RectangleF boundsF = bitmap.GetBounds(ref unit);
+            Width = (int) boundsF.Width;
+            Height = (int) boundsF.Height;
+            Rectangle bounds = new Rectangle((int)boundsF.X,
+                (int)boundsF.Y,
+                (int)boundsF.Width,
+                (int)boundsF.Height);
+            
+            // Figure out the number of bytes in a row
+            // This is rounded up to be a multiple of 4
+            // bytes, since a scan line in an image must always be a multiple of 4 bytes
+            // in length. 
+            dataWidth = (int)boundsF.Width * sizeof(PixelData);
+            if (dataWidth % 4 != 0)
+            {
+                dataWidth = 4 * (dataWidth / 4 + 1);
+            }
+            bitmapData =
+                bitmap.LockBits(bounds, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+            pBase = (Byte*)bitmapData.Scan0.ToPointer();
+
+            pixels = new PixelData[bitmap.Width, bitmap.Height];
+            for (var x = 0; x < Width; x++)
+            {
+                for (var y = 0; y < Height; y++)
+                {
+                    pixels[x, y] = *PixelAt(x, y);
+                }
+            }
+        }
+
+        public PixelData GetPixel(int x, int y)
+        {
+            return pixels[x, y];
+        }
+
+        public void SetPixel(int x, int y, PixelData colour)
+        {
+            PixelData* pixel = PixelAt(x, y);
+            *pixel = colour;
+        }
+
+        public void UnlockBitmap()
+        {
+            bitmap.UnlockBits(bitmapData);
+            bitmapData = null;
+            pBase = null;
+        }
+
+        public PixelData* PixelAt(int x, int y)
+        {
+            return (PixelData*)(pBase + y * dataWidth + x * sizeof(PixelData));
+        }
+    }
+    public struct PixelData
+    {
+        public byte blue;
+        public byte green;
+        public byte red;
+    }
+}
--- Part 2/ObjectTracker/VideoObjectTracker.cs
+++ Part 2/ObjectTracker/VideoObjectTracker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
@@ -11,6 +12,8 @@ namespace ObjectTracker
 {
     public class VideoObjectTracker
     {
+        private readonly double MOVEMENT_STANDARD_DEVIATIONS = 8;
+
         public void Process(string inputPath, string outputDirectory, string outputName)
         {
             Directory.CreateDirectory(outputDirectory);
@@ -25,14 +28,23 @@ namespace ObjectTracker
                         reader.UnderlyingReader.Height
                     );
 
-                    Bitmap lastBitmap = null;
+                    FastBitmap lastBitmap = null;
                     reader.ReadAllFrames(
                         (bitmap, index) =>
                         {
-                            ProcessFrame(bitmap, lastBitmap);
+                            if (index > 50)
+                            {
+                                return;
+                            }
+                            var fastBitmap = new FastBitmap(bitmap);
+                            fastBitmap.LockBitmap();
+                            var outputBitmap = ProcessFrame(fastBitmap, lastBitmap);
+                            fastBitmap.UnlockBitmap();
+                            writer.WriteVideoFrame(outputBitmap);
                             lastBitmap?.Dispose();
-                            writer.WriteVideoFrame(bitmap);
-                            lastBitmap = bitmap;
+                            lastBitmap = new FastBitmap(bitmap);
+                            lastBitmap.LockBitmap();
+                            Console.WriteLine($"Processed frame {index}");
                         }
                     );
                     lastBitmap.Dispose();
@@ -40,12 +52,80 @@ namespace ObjectTracker
             }
         }
 
-        private void ProcessFrame(Bitmap bitmap, Bitmap lastBitmap)
+        private Bitmap ProcessFrame(FastBitmap bitmap, FastBitmap lastBitmap)
         {
-            if (lastBitmap != null)
+            if (lastBitmap == null)
             {
-                bitmap.SetPixel(0, 0, Color.Red);
+                return bitmap.Bitmap;
             }
+
+            var colorDifferenceMatrix = new int[bitmap.Width, bitmap.Height];
+            long totalColorDifferences = 0;
+            for (var x = 0; x < bitmap.Width; x++)
+            {
+                for (var y = 0; y < bitmap.Height; y++)
+                {
+                    const int radius = 3;
+                    int difference = 0;
+                    for (var xOffset = -radius; xOffset <= radius; xOffset++)
+                    {
+                        for (var yOffset = -radius; yOffset <= radius; yOffset++)
+                        {
+                            var xOffsetSquared = xOffset * xOffset;
+                            var yOffsetSquared = yOffset * yOffset;
+                            var distance = Math.Sqrt(xOffsetSquared - yOffsetSquared);
+                            if (distance <= radius)
+                            {
+                                difference += GetPixelDifference(bitmap, lastBitmap, x + xOffset, y + yOffset);
+                            }
+                        }
+                    }
+                        
+                    colorDifferenceMatrix[x, y] = difference;
+                    totalColorDifferences += difference;
+                }
+            }
+
+            double meanDifference = (double) totalColorDifferences / (bitmap.Width * bitmap.Height);
+            double sumOfSquaredMeanDifferences = 0;
+            for (var x = 0; x < colorDifferenceMatrix.GetLength(0); x++)
+            {
+                for (var y = 0; y < colorDifferenceMatrix.GetLength(1); y++)
+                {
+                    var dif = colorDifferenceMatrix[x, y] - meanDifference;
+                    sumOfSquaredMeanDifferences += dif * dif;
+                }
+            }
+
+            double standardDeviation = Math.Sqrt(1d / (bitmap.Width * bitmap.Height) * sumOfSquaredMeanDifferences);
+            
+            for (var x = 0; x < colorDifferenceMatrix.GetLength(0); x++)
+            {
+                for (var y = 0; y < colorDifferenceMatrix.GetLength(1); y++)
+                {
+                    var colorDifference = colorDifferenceMatrix[x, y];
+                    
+                    if (colorDifference >= standardDeviation * MOVEMENT_STANDARD_DEVIATIONS)
+                    {
+                        bitmap.SetPixel(x, y, new PixelData{ red = 255, green = 127, blue = 255 });
+                    }
+                }
+            }
+            return bitmap.Bitmap;
+        }
+
+        private int GetPixelDifference(FastBitmap bitmap, FastBitmap lastBitmap, int x, int y)
+        {
+            if (x < 0 || y < 0 || x >= bitmap.Width || y >= bitmap.Height)
+            {
+                return 0;
+            }
+
+            var color = bitmap.GetPixel(x, y);
+            var lastColor = lastBitmap.GetPixel(x, y);
+            return Math.Abs(color.red - lastColor.red)
+                             + Math.Abs(color.green - lastColor.green)
+                             + Math.Abs(color.blue - lastColor.blue);
         }
     }
 }
