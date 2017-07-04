63bc8d0309304c2c7ea647f75bfe9185c280d0d9
Dalton Spillman <dalton_spillman@yahoo.com>
2017-07-04 10:26:08 -0500

    Moved source code into Source directory

--- Source/Part 1/AStar/Run.cpp
+++ Source/Part 1/AStar/Run.cpp
@@ -10,6 +10,8 @@ void main() {
 	grid->setTraversalCost('|', -1);
 	grid->setTraversalCost('X', -1);
 	grid->setTraversalCost(' ', 1);
+	grid->setTraversalCost('S', 1);
+	grid->setTraversalCost('E', 1);
 	grid->set(
 		"------------"
 		"|          |"
@@ -23,17 +25,22 @@ void main() {
 		"|          |"
 		"|           "
 		"------------");
-
 	Loc start = Loc(1, 1);
 	Loc end = Loc(11, 10);
+	grid->set(start.getX(), start.getY(), 'S');
+	grid->set(end.getX(), end.getY(), 'E');
+
+	cout << "Starting grid:" << endl;
+	grid->print();
+
 	AStar* pathfinder = new AStar(grid, start, end);
 	vector<Loc*>* path = pathfinder->findPath();
 	for(int i = 0; i < path->size(); i++) {
 		Loc* loc = (*path)[i];
 		grid->set(loc->getX(), loc->getY(), '.');
 	}
-	grid->set(start.getX(), start.getY(), 'S');
-	grid->set(end.getX(), end.getY(), 'E');
+	
+	cout << endl << "With pathfinding:" << endl;
 	grid->print();
 
 	cout << endl;
